96fafaf581084312c181129bf256160fe20cc73e
Shreeparna Mitra <preetimitra.bell@gmail.com>
2025-09-29 15:41:41 +0530

    Fix CORS for production frontend

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,12 @@ const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve()
 
 //middleware
-if(process.env.NODE_ENV !== "production"){
-    app.use(cors({
-    origin: "http://localhost:5173",
-}))
-}
+// Enable CORS for all environments
+app.use(cors({
+    origin: ["https://thinkboard-frontend-vqvx.onrender.com", "http://localhost:5173"],
+    credentials: true
+}));
+
 app.use(express.json()); //this middleware will parse JSON bodies: get access to req.body
 
 // app.use(rateLimiter)
@@ -32,10 +33,6 @@ app.use(express.json()); //this middleware will parse JSON bodies: get access to
 // })
 
 app.use("/api/notes",notesRoutes);
-
-
-
-
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/Thinkboard/dist")))
